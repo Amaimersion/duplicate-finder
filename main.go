83e087797e66875e5664f9273432de1857d08828
Sergey Kuznetsov <amatsukine@gmail.com>
2022-01-14 22:09:47 +0300

    config check: edit eror message

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func (c config) check() error {
 	}
 
 	if c.folder1 == c.folder2 {
-		return errors.New("same folder not supported, instead copy f1 in new folder")
+		return errors.New("same folder not supported, instead copy f1 to new folder")
 	}
 
 	return nil
